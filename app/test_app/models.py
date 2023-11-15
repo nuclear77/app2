a10abf7f8c34d59d8664731c6798cb6a61935e5e
nuclear7777 <spiritvoideu@gmail.com>
2023-11-15 19:27:11 +0300

    changed

--- app/test_app/models.py
+++ app/test_app/models.py
@@ -14,10 +14,10 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.ManyToManyRel)
-    library = models.ForeignKey(Library, on_delete=models.ManyToOneRel)
+    author = models.ForeignKey(Author, on_delete=models.OneToOneField)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
 
 class LibraryBook(models.Model):
-    library = models.ForeignKey(Library, on_delete=models.ManyToOneRel)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
     books = models.ManyToManyField(Book)
